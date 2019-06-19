0415c415dccc7c0c4071e74b31f5ff4c77590604
ufukmehmetoglu <ufuk.mehmetoglu@gmail.com>
2019-06-19 16:54:31 -0700

    Update

--- server/index.js
+++ server/index.js
@@ -20,9 +20,9 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.use('/', proxy({
   target: 'ec2-3-14-5-216.us-east-2.compute.amazonaws.com',
   router: {
-    '/navbar': 'http://ec2-54-153-22-73.us-west-1.compute.amazonaws.com/navbar/mongo/search/9000000',
+    '/navbar': 'http://ec2-54-153-22-73.us-west-1.compute.amazonaws.com',
     '/productDescription': 'http://ec2-18-188-70-40.us-east-2.compute.amazonaws.com',
-    '/morelooks': 'http://ec2-18-191-169-48.us-east-2.compute.amazonaws.com/morelooks/api',
+    '/morelooks': 'http://ec2-18-191-169-48.us-east-2.compute.amazonaws.com',
     '/reviews': 'http://ec2-18-191-154-4.us-east-2.compute.amazonaws.com'
   }
 }))
